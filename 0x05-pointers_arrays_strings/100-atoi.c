c98ab4a50959f983f1e962e2fdcd7eae220455e3
MikiastesfayeJ <Mikiastesfaye998@gmail.com>
2022-03-21 09:42:21 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,7 +5,7 @@
  *
  * Return: value of s
  */
-int _atoi(char *S)
+int _atoi(char *s)
 {
 	int i, sign, numb;
 
@@ -19,7 +19,7 @@ int _atoi(char *S)
 			sign *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			while (s[i] > = '0' && s[i] <= '9')
+			while (s[i] >= '0' && s[i] <= '9')
 			{
 				numb = (s[i] - '0') * sign + numb * 10;
 				i++;
