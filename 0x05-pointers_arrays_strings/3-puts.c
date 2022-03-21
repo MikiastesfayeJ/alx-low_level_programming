52dbb2f0366d80b3385d3506111cb0a77e7262b7
MikiastesfayeJ <Mikiastesfaye998@gmail.com>
2022-03-21 08:20:11 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "coding.h"
+/**
+ * _puts - function with one argument
+ * @str: one char type argument
+ *
+ * Return: none
+ */
+void _puts(char *str)
+{
+	while(*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+
