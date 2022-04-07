55d021bd497ff694e2eaa6425acc1c68483b165f
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-04-07 23:58:59 +0300

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,115 +1,37 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
- *
- * Return: 1 if digit, 0 otherwise
+ * _realloc -  reallocates a memory block using malloc and free
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new size
+ * Return: pointer
  */
-int _isdigit(int c)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	return (c >= '0' && c <= '9');
-}
+	char *clone, *relloc;
+	unsigned int i;
 
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * big_multiply - multiply two big number strings
- * @num1: the first big number string
- * @num2: the second big number string
- *
- * Return: the product big number string
- */
-char *big_multiply(char *num1, char *num2)
-{
-	char *mul;
-	int l1, l2, a, b, c, x;
-
-	l1 = _strlen(num1);
-	l2 = _strlen(num2);
-	mul = malloc(a = x = l1 + l2);
-	if (!mul)
-		printf("Error\n"), exit(98);
-	while (a--)
-		mul[a] = 0;
-
-	for (l1--; l1 >= 0; l1--)
-	{
-		if (!_isdigit(num1[l1]))
-		{
-			free(mul);
-			printf("Error\n"), exit(98);
-		}
-		a = num1[l1] - '0';
-		c = 0;
-
-		for (l2 = _strlen(num2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(num2[l2]))
-			{
-				free(mul);
-				printf("Error\n"), exit(98);
-			}
-			b = num2[l2] - '0';
-
-			c += mul[l1 + l2 + 1] + (a * b);
-			mul[l1 + l2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			mul[l1 + l2 + 1] += c;
+	if (ptr != NULL)
+		clone = ptr;
+	else
+	{ 
+		return (malloc(new_size));
 	}
-	return (mul);
-}
-
-
-/**
- * main - multiply two big number strings
- * @argc: the number of arguments
- * @argv: the argument vector
- *
- * Return: Always 0 on success.
- */
-int main(int argc, char **argv)
-{
-	char *mul;
-	int a, c, x;
-
-	if (argc != 3)
-		printf("Error\n"), exit(98);
-
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	mul = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
 	{
-		if (mul[c])
-			a = 1;
-		if (a)
-			_putchar(mul[c] + '0');
-		c++;
+		free(ptr);
+		return (0);
 	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(mul);
-	return (0);
+	relloc = malloc(new_size);
+	if (relloc == NULL)
+		return (0);
+	for (i = 0; i < (old_size || i < new_size); i++)
+	{
+		*(relloc + i) = clone[i];
+	}
+	free(ptr);
+	return (relloc);
 }
