52bd8dbd25c44548fe284aa91c1f9e350f91b3ea
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-04-27 00:35:23 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * print_listint_safe - prints a linked list, safely
  * @head: list of type listint_t to print
@@ -16,9 +17,7 @@ size_t print_listint_safe(const listint_t *head)
         num++;
         printf("[%p] %d\n", (void *)head, head->n);
         if (diff > 0)
-        {
             head = head->next;
-        }
         else
         {
             printf("-> [%p] %d\n", (void *)head->next, head->next->n);
